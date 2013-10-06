d677c7bd9e71a55137fae64b26c3c4de0cbbc3e8
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-10-06 18:03:08 +0200

    Cleaned up block code

--- com/triniumrpg/blocks/TriniumBlock.java
+++ com/triniumrpg/blocks/TriniumBlock.java
@@ -10,9 +10,9 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
-public class BlockRelphiteOre extends Block{
+public class TriniumBlock extends Block{
 
-	public BlockRelphiteOre(int par1, Material par2Material) {
+	public TriniumBlock(int par1, Material par2Material) {
 		super(par1, par2Material);
 		setCreativeTab(TriniumRPG.tabTrinium);
 	}
@@ -24,5 +24,3 @@ public class BlockRelphiteOre extends Block{
 		this.blockIcon = par1IconRegister.registerIcon(ModInfo.ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(5));
 	}
 }
-
-
