f9bd9fe1d343f721b8d6f60c410141d0887ecde9
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-10-06 18:04:06 +0200

    Added config file

--- com/triniumrpg/handlers/ConfigHandler.java
+++ com/triniumrpg/handlers/ConfigHandler.java
@@ -14,8 +14,20 @@ public class ConfigHandler {
 		config.load();
 
 		IDs.goldencoin_actual = config.getItem(Names.goldencoin_name, IDs.goldencoin_default).getInt() - 256;
+		IDs.beefStew_actual = config.getItem(Names.beefStew_name, IDs.beefStew_default).getInt() - 256;
+		IDs.adminiumIngot_actual = config.getItem(Names.adminiumIngot_name, IDs.adminiumIngot_default).getInt() - 256;
+		IDs.runiumIngot_actual = config.getItem(Names.runiumIngot_name, IDs.runiumIngot_default).getInt() - 256;
+		IDs.relphiteDust_actual = config.getItem(Names.relphiteDust_name, IDs.relphiteDust_default).getInt() - 256;
+		IDs.adminiumSword_actual = config.getItem(Names.adminiumSword_name, IDs.adminiumSword_default).getInt() - 256;
+		IDs.runiumSword_actual = config.getItem(Names.runiumSword_name, IDs.runiumSword_default).getInt() - 256;
 
-		//IDs.smithinganvil_actual = config.getBlock(Names.smithinganvil_name, IDs.smithinganvil_default).getInt();
+		
+		IDs.adminiumOre_actual = config.getBlock(Names.adminiumOre_name, IDs.adminiumOre_default).getInt();
+		IDs.runiumOre_actual = config.getBlock(Names.runiumOre_name, IDs.runiumOre_default).getInt();
+		IDs.relphiteOre_actual = config.getBlock(Names.relphiteOre_name, IDs.relphiteOre_default).getInt();
+		IDs.relphiteFrame_actual = config.getBlock(Names.relphiteFrame_name, IDs.relphiteFrame_default).getInt();
+		IDs.relphitianSoil_actual = config.getBlock(Names.relphitianSoil_name, IDs.relphitianSoil_default).getInt();
+		IDs.relphiteStone_actual = config.getBlock(Names.relphiteStone_name, IDs.relphiteStone_default).getInt();
 
 		config.save();
 	}
