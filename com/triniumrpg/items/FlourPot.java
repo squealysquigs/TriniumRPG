ab9c6fef3383782a9478e8e7a69b4e81facbd7a4
Lucas Olai Jarlkov Olsen <gamingtutsdk@gmail.com>
2013-10-07 17:27:27 +0200

    Added realistic bread recipe.

---
+++ com/triniumrpg/items/FlourPot.java
@@ -0,0 +1,60 @@
+package com.triniumrpg.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+public class FlourPot extends TriniumItem
+{
+    public FlourPot(int par1)
+    {
+        super(par1);
+        this.setMaxStackSize(1);
+    }
+
+    public ItemStack onEaten(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        if (!par3EntityPlayer.capabilities.isCreativeMode)
+        {
+            --par1ItemStack.stackSize;
+        }
+
+        if (!par2World.isRemote)
+        {
+            par3EntityPlayer.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 1800, 1));
+            par3EntityPlayer.addPotionEffect(new PotionEffect(Potion.hunger.id, 1800, 1));
+            par3EntityPlayer.addChatMessage("Wow! That taste awful!");
+        }
+
+        return par1ItemStack.stackSize <= 0 ? new ItemStack(Items.pot) : par1ItemStack;
+    }
+
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getMaxItemUseDuration(ItemStack par1ItemStack)
+    {
+        return 32;
+    }
+
+    /**
+     * returns the action that specifies what animation to play when the items is being used
+     */
+    public EnumAction getItemUseAction(ItemStack par1ItemStack)
+    {
+        return EnumAction.drink;
+    }
+
+    /**
+     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
+     */
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        return par1ItemStack;
+    }
+}
